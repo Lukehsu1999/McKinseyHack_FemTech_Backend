5369ffd018e2880da885ea9d87c4fcd4579a7f5f
Look! <lukehsu1999@gmail.com>
2023-07-15 14:46:11 -0400

    create_event: invoke func_sendEmail

--- lambdas/femtech_create_event.py
+++ lambdas/femtech_create_event.py
@@ -4,7 +4,28 @@ import boto3
 def lambda_handler(event, context):
     # TODO implement
     print(event)
+    
+    # Create an AWS Lambda client
+    lambda_client = boto3.client('lambda')
+    
+    # parameters for email
+    parameter = {
+        "recipients": ["th2881@columbia.edu"],
+        "msg": event
+    }
+    
+    # Invoke the target Lambda function
+    response = lambda_client.invoke(
+        FunctionName='arn:aws:lambda:us-east-1:238592221641:function:func_sendEmail',
+        InvocationType='Event',  # or 'RequestResponse' for synchronous invocation
+        Payload=json.dumps(parameter)  # Optional payload to pass to the target Lambda function
+    )
+    
+    
+    
+    
     return {
         'statusCode': 200,
         'body': event
     }
+    
