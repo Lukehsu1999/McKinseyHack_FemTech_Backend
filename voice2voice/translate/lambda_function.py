ff280074cff0208330ee19d5e4a2d8e57e2b4d33
Look! <lukehsu1999@gmail.com>
2023-07-16 05:47:56 -0400

    func_translate: implemented

--- voice2voice/translate/lambda_function.py
+++ voice2voice/translate/lambda_function.py
@@ -4,13 +4,15 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    translator= Translator(to_lang="en", from_lang="zh")
-    translation = translator.translate("你好")
-    print(translation)
+    translator= Translator(to_lang=event["to_lang"], from_lang=event["from_lang"])
+    translation = translator.translate(event["msg"])
+
+    print("from_lang: ", event["from_lang"])
+    print("to_lang: ", event["to_lang"])
+    print("msg: ", event["msg"])
+    print("translation: ", translation)
 
     return {
         'statusCode': 200,
         'body': translation
     }
-
-lambda_handler({},{})
