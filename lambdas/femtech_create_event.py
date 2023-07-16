6c86763d7ac3682e782a528973f71dff8737bd89
Look! <lukehsu1999@gmail.com>
2023-07-16 03:01:10 -0400

    create_event: filtered version & format msg

--- lambdas/femtech_create_event.py
+++ lambdas/femtech_create_event.py
@@ -1,17 +1,101 @@
 import json
 import boto3
 
+dataset = [
+  {
+    "uid": "1",
+    "first name": "Abhimanyu",
+    "last name": "Swaroop",
+    "email": "as6434@columbia.edu",
+    "number": "+12129616591",
+    "moc": "Whatsapp",
+    "user filter": "fintech"
+  },
+  {
+    "uid": "2",
+    "first name": "Anvi",
+    "last name": "Agarwal",
+    "email": "anvi.agarwal@columbia.edu",
+    "number": "",
+    "moc": "email",
+    "user filter": "fintech"
+  },
+  {
+    "uid": "3",
+    "first name": "Luke",
+    "last name": "Hsu",
+    "email": "th2881@columbia.edu",
+    "number": "",
+    "moc": "email",
+    "user filter": "fintech"
+  },
+  {
+    "uid": "4",
+    "first name": "Elena",
+    "last name": "",
+    "email": "pexy@seas.upenn.edu",
+    "number": "+15512207773",
+    "moc": "SMS",
+    "user filter": "fintech"
+  },
+  {
+    "uid": "5",
+    "first name": "AbhiTwo",
+    "last name": "Swaroop",
+    "email": "as6434@columbia.edu",
+    "number": "",
+    "moc": "email",
+    "user filter": "micro"
+  },
+  {
+    "uid": "6",
+    "first name": "ElenTwo",
+    "last name": "",
+    "email": "pexy@seas.upenn.edu",
+    "number": "",
+    "moc": "email",
+    "user filter": "micro"
+  }
+]
+
+
 def lambda_handler(event, context):
+    # Convert the JSON data to lowercase for 'moc' values
+    for item in dataset:
+        item['moc'] = item['moc'].lower()
+
+    # Extract user filters from the event object
+    user_filters = event['userFilter']
+    
+
+    # Filter the data based on user filters
+    filtered_data = []
+    for user in dataset:
+        if user['user filter'] in user_filters:
+            filtered_data.append(user)
+    
+    print("filtered_data: ",filtered_data)
+    # Extract phone numbers for SMS
+    sms_list = [item['number'] for item in filtered_data if item['moc'] == 'sms' and item['number'] is not None]
+
+    # Extract phone numbers for WhatsApp
+    whatsapp_list = ['whatsapp:' + item['number'] for item in filtered_data if item['moc'] == 'whatsapp' and item['number'] is not None]
+
+    # Extract email addresses
+    email_list = [item['email'] for item in filtered_data if item['moc'] == 'email' and item['email'] is not None]
+
+    msg = 'Title: '+event['title']+'\n'+'Event Description: '+event['description']+'\n'+'When: '+event['time']+'\n'+'Where: '+event['meetinglink']
     # TODO implement
-    print(event)
+    print(whatsapp_list, email_list, sms_list)
+    print("msg: ", msg)
     
     # Create an AWS Lambda client
     lambda_client = boto3.client('lambda')
     
     # parameters for email
     email_parameter = {
-        "recipients": ["th2881@columbia.edu", "anvi.agarwal@columbia.edu"],
-        "msg": json.dumps(event)
+        "recipients": email_list,
+        "msg": msg
     }
     
     # Invoke the target Lambda function
@@ -21,13 +105,11 @@ def lambda_handler(event, context):
         Payload=json.dumps(email_parameter)  # Optional payload to pass to the target Lambda function
     )
     
-    print("Response from send Email", response)
-    
     # parameters for whatsapp
     sms_parameter = {
         "sender": "+18776586958",
-        "recipients": ["+15512207773"],
-        "msg": json.dumps(event)
+        "recipients": sms_list,
+        "msg": msg
     }
     
     response = lambda_client.invoke(
@@ -41,8 +123,8 @@ def lambda_handler(event, context):
     # parameters for whatsapp
     whatsapp_parameter = {
         "sender": "whatsapp:+14155238886",
-        "recipients": ["whatsapp:+12129616591"],
-        "msg": json.dumps(event)
+        "recipients": whatsapp_list,
+        "msg": msg
     }
     
     response = lambda_client.invoke(
